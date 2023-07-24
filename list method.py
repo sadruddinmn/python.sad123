c90b313c9256ca81f7cd683fa372f8fa58d4fbc1
sadrudinmn <sadruddinmominzadaa@gmail.com>
2023-07-24 19:26:42 +0500

    code for list methods

---
+++ list method.py
@@ -0,0 +1,48 @@
+"""fruits = ["apply","orange","cherry"];
+fruits.append("mango")
+print(fruits)""
+
+alphabet = ["F","S","J"]
+alphabet.clear()
+print(alphabet)""
+
+x = [1,4,6]
+b = x.copy()
+print(x)""
+
+numbers = [9,6,3,9]
+x=numbers.count(9)
+print(x)""
+
+fruits = ["apply","orange","mango"]
+number = [3,8,6]
+number.extend(fruits)
+print(number)""
+
+room = ["diningroom","bathroom","livingroom"]
+x = room.index("bathroom")
+print(x)""
+
+name = ["shah","moon","khan"]
+name.insert(1,"jan")
+print(name)""
+
+number = [4,3,6,9]
+number.pop(2)
+print(number)""
+
+
+number= [56,89,98]
+number.remove(56)
+print(number)""
+
+
+number= [56,89,98]
+number.reverse()
+print(number)"""
+
+
+number= [56,89,98,4,8,1]
+number.sort()
+print(number)
+
